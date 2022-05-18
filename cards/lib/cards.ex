800837400052b948d63e04b2682aa386c81e099a
Parker Catalano <parkercatalano@Parkers-MBP.attlocal.net>
2022-05-18 17:50:12 -0400

    Completed section 2.

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -23,4 +23,22 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "#{filename} does not exist."
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+  end
 end
