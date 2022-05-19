8be958987916681c386cca351dee3a1c761b5fd2
Parker Catalano <parkercatalano@Parkers-MBP.attlocal.net>
2022-05-19 13:01:51 -0400

    Section 4 and 5 complete.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,11 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
+  def main(input) do
+    input
+    |> hash_input()
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
   end
 end
