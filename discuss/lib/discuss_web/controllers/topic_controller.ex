f735d68799538d1a0ef3df23b6b6c993224215af
Parker Catalano <parkercatalano@Parkers-MBP.attlocal.net>
2022-09-14 12:24:15 -0400

    Created index page.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,12 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render(conn, "index.html", topics: topics)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -17,7 +17,7 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through(:browser)
 
-    get("/", PageController, :index)
+    get("/", TopicController, :index)
     get("/topics/new", TopicController, :new)
     post("/topics", TopicController, :create)
   end
