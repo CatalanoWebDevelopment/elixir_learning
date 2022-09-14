f77f99f1c1a6449d6006f885910cb5b62309e676
Parker Catalano <parkercatalano@Parkers-MBP.attlocal.net>
2022-09-14 13:32:22 -0400

    Created add button to route to create page.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -20,7 +20,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} -> render(conn, "new.html", changeset: changeset)
     end
   end
