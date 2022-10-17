3b23f59238374971b66d3818c93ab7e4e71ebfc6
Parker Catalano <parkercatalano@Parkers-MBP.attlocal.net>
2022-10-17 13:47:06 -0400

    Finish edit functionality.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -20,7 +20,7 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, _post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
@@ -33,5 +33,18 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(topic)
 
     render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} -> render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
   end
 end
