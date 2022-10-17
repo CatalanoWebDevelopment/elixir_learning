6063f3b022137f298ef3a8ec7bca86e4f4e512f5
Parker Catalano <parkercatalano@Parkers-MBP.attlocal.net>
2022-10-17 14:10:06 -0400

    Finished delete functionality.

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -47,4 +47,13 @@ defmodule DiscussWeb.TopicController do
       {:error, changeset} -> render(conn, "edit.html", changeset: changeset, topic: old_topic)
     end
   end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id)
+    |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
 end
