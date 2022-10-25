cf8b84d5353e68b5a51378ca476aac7ea755eabc
CatalanoWebDevelopment <catalano.work@gmail.com>
2022-10-25 13:13:18 -0400

    Persist user to DB in AuthController.

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,8 +2,22 @@ defmodule Discuss.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.inspect(conn.assigns)
-    IO.inspect(params)
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{provider: provider} = params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: provider}
+    changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update_user(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+
+      user ->
+        {:ok, user}
+    end
   end
 end
