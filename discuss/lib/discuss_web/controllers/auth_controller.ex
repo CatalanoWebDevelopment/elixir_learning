5d937d3db1e9146cda0ca8b11dac8e5a086ec550
CatalanoWebDevelopment <catalano.work@gmail.com>
2022-10-25 13:29:00 -0400

    Handled OAuth success callback.

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -3,12 +3,28 @@ defmodule Discuss.AuthController do
   plug Ueberauth
 
   alias Discuss.User
+  alias Discuss.Repo
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{provider: provider} = params) do
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider} = params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: provider}
     changeset = User.changeset(%User{}, user_params)
 
-    insert_or_update_user(changeset)
+    sign_in(conn, changeset)
+  end
+
+  defp sign_in(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in.")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
